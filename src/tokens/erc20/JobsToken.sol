ce88bd758c28c5b3a4e8ddfdcf446c5e11b60ef6
Lovro Posel <lovro.posel79@gmail.com>
2025-09-09 22:16:24 +0200

    JobsToken.sol

--- src/tokens/erc20/JobsToken.sol
+++ src/tokens/erc20/JobsToken.sol
@@ -1,1 +1,11 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract JobsToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("JobsToken", "JOBS") {
+        _mint(msg.sender, initialSupply * (10 ** decimals()));
+    }
+}
