2a6c51926d3d3f03e0147204dfc9ffb352e8b046
Lovro Posel <lovro.posel79@gmail.com>
2025-09-09 22:21:07 +0200

    JobsNFT.sol

--- src/tokens/erc721/JobsNFT.sol
+++ src/tokens/erc721/JobsNFT.sol
@@ -1,1 +1,16 @@
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract JobsNFT is ERC721URIStorage, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor() ERC721("JobsNFT", "JNFT") Ownable(msg.sender) {}
+
+    function mint(address to, string memory tokenURI) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+    }
+}
